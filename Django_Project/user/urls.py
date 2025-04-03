9b10f5a9f895bfa93cb6c908c326115ed6bdc4b0
JunHo-L <wnsgh4368@naver.com>
2025-04-04 01:02:39 +0900

    user 로그아웃, 회웑정보 조회,수정,탈퇴 API구현

--- Django_Project/user/urls.py
+++ Django_Project/user/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
-from .views import RegisterView
+from .views import RegisterView , LogoutView , UserDetailUpdateView , UserDeleteView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('me/', UserDetailUpdateView.as_view(), name='user_profile'),
+    path('delete/', UserDeleteView.as_view(), name='user_delete'),
 ]
